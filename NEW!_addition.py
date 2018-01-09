a77604af3ecc98d5a03846a2fb682f0c010ea9ee
Valaya Choudhary <LukevaderG@Harshs-MacBook-Air.local>
2018-01-08 21:17:30 -0500

    HI

---
+++ NEW!_addition.py
@@ -0,0 +1,5 @@
+from sys import argv
+script, first, second, third = argv
+#number3 = number1 + number2 
+print first + second
+print second
---
+++ argv.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, number1,number2= argv
+number1 = int (number1)
+number2 = int (number2)
+print "This is number " , number1
+
+print "This is number " , number2
+
+print number1 + number2
