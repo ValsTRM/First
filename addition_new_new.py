cced0a666fc35e96c4e298cc1178c46531770782
FIREREX AWESOME <FIREREX@hotmail.comgit config --global user.name FIREREX>
2018-01-20 22:53:27 -0500

    3 more files!

--- addition_new_new.py
+++ addition_new_new.py
@@ -1,4 +1,7 @@
+num1 = int(raw_input("input number "))
+num2 = int(raw_input("input second number "))
 def add (arg1, arg2):
-	print int(arg1 + arg2)
 
-add(9, 10)
+	print int(arg1 + arg2)
+	
+add(num1, num2)
